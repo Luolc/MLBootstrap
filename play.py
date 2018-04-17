e1f3d8e2b5c57f26fe6d1a654309ae3452135713
Luolc <luolc.witty@gmail.com>
2018-04-17 21:37:35 +0800

    update data process

--- play.py
+++ play.py
@@ -2,83 +2,86 @@ from process import DataPreprocessor
 from mlbootstrap import Bootstrap
 import tensorflow as tf
 
-bootstrap = Bootstrap('config.yaml', preprocessor=DataPreprocessor())
-bootstrap.preprocess(force=True)
-data = bootstrap.dataset()
-max_doc_length = data['max_doc_length']
-embedding_size = 300
+bootstrap = Bootstrap(
+    'config.yaml',
+    preprocessor=DataPreprocessor())
+bootstrap.preprocess(force=False)
 
-with tf.name_scope('input'):
-    title = tf.placeholder(tf.int32, [None, max_doc_length], name='title')
-    significant = tf.placeholder(tf.float32, [None], name='significant')
-    target = tf.placeholder(tf.float32, [None], name='target')
+# data = bootstrap.dataset()
+# max_doc_length = data['max_doc_length']
+# embedding_size = 300
 
+# with tf.name_scope('input'):
+#     title = tf.placeholder(tf.int32, [None, max_doc_length], name='title')
+#     significant = tf.placeholder(tf.float32, [None], name='significant')
+#     target = tf.placeholder(tf.float32, [None], name='target')
+#
+#
+# def weighted_variable(shape, name=None):
+#     initializer = tf.random_uniform_initializer(-0.1, 0.1)
+#     return tf.Variable(initializer(shape), name=name)
+#
+#
+# def bias_variable(shape):
+#     return tf.Variable(tf.constant(.0, shape=shape))
+#
+#
+# with tf.name_scope('title'):
+#     W = weighted_variable([data['vocab'], embedding_size])
+#     title_emb = tf.reduce_sum(tf.nn.embedding_lookup(W, title), axis=1)
+#
+# with tf.name_scope('attention'):
+#     A = weighted_variable([1, embedding_size])
+#     attention = tf.reduce_sum(title_emb * A, axis=1)
+#
+# with tf.name_scope('score'):
+#     W_fc = weighted_variable([2, 1])
+#     B_fc = bias_variable([1])
+#     score = tf.squeeze(tf.transpose(tf.stack([attention, significant])) @ W_fc + B_fc)
+#
+# with tf.name_scope('loss'):
+#     l2_loss = tf.nn.l2_loss(score - target)
+#     loss_op = l2_loss
+#
+# with tf.name_scope('train'):
+#     optimizer = tf.train.AdamOptimizer()
+#     train_op = optimizer.minimize(loss_op)
+#
+# with tf.name_scope('predict'):
+#     _, indices = tf.nn.top_k(score, 3)
+#     best_possible = tf.reduce_sum(tf.nn.top_k(target, 3)[0])
+#     predict_op = tf.reduce_sum(tf.gather(target, indices)) / best_possible
+#     _, indices = tf.nn.top_k(significant, 3)
+#     baseline_op = tf.reduce_sum(tf.gather(target, indices)) / best_possible
 
-def weighted_variable(shape, name=None):
-    initializer = tf.random_uniform_initializer(-0.1, 0.1)
-    return tf.Variable(initializer(shape), name=name)
-
-
-def bias_variable(shape):
-    return tf.Variable(tf.constant(.0, shape=shape))
-
-
-with tf.name_scope('title'):
-    W = weighted_variable([data['vocab'], embedding_size])
-    title_emb = tf.reduce_sum(tf.nn.embedding_lookup(W, title), axis=1)
-
-with tf.name_scope('attention'):
-    A = weighted_variable([1, embedding_size])
-    attention = tf.reduce_sum(title_emb * A, axis=1)
-
-with tf.name_scope('score'):
-    W_fc = weighted_variable([2, 1])
-    B_fc = bias_variable([1])
-    score = tf.squeeze(tf.transpose(tf.stack([attention, significant])) @ W_fc + B_fc)
-
-with tf.name_scope('loss'):
-    l2_loss = tf.nn.l2_loss(score - target)
-    loss_op = l2_loss
-
-with tf.name_scope('train'):
-    optimizer = tf.train.AdamOptimizer()
-    train_op = optimizer.minimize(loss_op)
-
-with tf.name_scope('predict'):
-    _, indices = tf.nn.top_k(score, 3)
-    best_possible = tf.reduce_sum(tf.nn.top_k(target, 3)[0])
-    predict_op = tf.reduce_sum(tf.gather(target, indices)) / best_possible
-    _, indices = tf.nn.top_k(significant, 3)
-    baseline_op = tf.reduce_sum(tf.gather(target, indices)) / best_possible
-
-session = tf.Session()
-for step in range(1000):
-    session.run(tf.global_variables_initializer())
-    for batch in data['train']:
-        session.run([train_op], feed_dict={
-            title: batch['title'], significant: batch['significant'], target: batch['target']
-        })
-    if step % 50 == 0:
-        print('---------')
-        print('Epoch {}'.format(step))
-
-
-        def __print_validation(mode):
-            predict = 0
-            baseline = 0
-            for __batch in data[mode]:
-                pred, ba = session.run([predict_op, baseline_op], feed_dict={
-                    title: __batch['title'], significant: __batch['significant'],
-                    target: __batch['target']
-                })
-                predict += pred
-                baseline += ba
-            predict /= len(data['train'])
-            baseline /= len(data['train'])
-            print('{} predict: {}'.format(mode, predict))
-            print('{} baseline: {}'.format(mode, baseline))
-
-
-        __print_validation('train')
-        __print_validation('val')
-        __print_validation('test')
+# session = tf.Session()
+# for step in range(1000):
+#     session.run(tf.global_variables_initializer())
+#     for batch in data['train']:
+#         session.run([train_op], feed_dict={
+#             title: batch['title'], significant: batch['significant'], target: batch['target']
+#         })
+#     if step % 50 == 0:
+#         print('---------')
+#         print('Epoch {}'.format(step))
+#
+#
+#         def __print_validation(mode):
+#             predict = 0
+#             baseline = 0
+#             for __batch in data[mode]:
+#                 pred, ba = session.run([predict_op, baseline_op], feed_dict={
+#                     title: __batch['title'], significant: __batch['significant'],
+#                     target: __batch['target']
+#                 })
+#                 predict += pred
+#                 baseline += ba
+#             predict /= len(data['train'])
+#             baseline /= len(data['train'])
+#             print('{} predict: {}'.format(mode, predict))
+#             print('{} baseline: {}'.format(mode, baseline))
+#
+#
+#         __print_validation('train')
+#         __print_validation('val')
+#         __print_validation('test')
--- process.py
+++ process.py
@@ -1,80 +1,99 @@
 from mlbootstrap.preprocess import BasicPreprocessor
+from pathlib import Path
 import os
 import shutil
 import json
 import numpy as np
 import pandas as pd
-import tensorflow as tf
+import tflearn
+
+_TABLE_INSIGHT_DIR = 'table-insight'
+_VOCAB_FILENAME = 'vocab_processor.obj'
+_DATASET_META = 'meta.json'
+
+_STOP_WORDS = {' ', 'a', 'an', 'the', 'and'}
+_TYPE2ID = {'rate': 0, 'trend': 1}
+
+
+def _parse_insight(fp):
+    with open(fp, 'r') as f:
+        result = json.load(f)
+        for item in result:
+            item['title'] = ' '.join(
+                w.lower() for w in item['title'] if w and w not in _STOP_WORDS)
+        return [item for item in result if item['title']]
+
+
+def _parse_table(fp):
+    return pd.read_csv(fp).as_matrix()
 
 
 class DataPreprocessor(BasicPreprocessor):
-    _VOCAB_FILENAME = 'vocab_processor.obj'
-    _DATASET_META = 'meta.json'
-
     def _on_next(self, src, dst, task):
-        shutil.rmtree(dst)
-        os.makedirs(dst)
+        # if Path(dst).exists():
+        #     shutil.rmtree(dst)
+        os.makedirs(dst, exist_ok=True)
 
-        stop_words = {'a', 'an', 'the'}
         raw_data = {}
-        for filename in os.listdir(src):
-            def __parse_insight(fp):
-                with open(fp, 'r') as f:
-                    result = json.load(f)
-                    for item in result:
-                        item['title'] = item['title'].replace('|', ' ').lower()
-                        item['title'] = ' '.join(
-                            w for w in item['title'].split() if w not in stop_words)
-                    return result
-
-            def __parse_table(fp):
-                return pd.read_csv(fp).as_matrix()[:, 1:5]
-
-            file_path = os.path.join(src, filename)
-            company = filename.replace('.txt', '').replace('.csv', '')
-            if company not in raw_data:
-                raw_data[company] = {}
+        for filename in os.listdir(os.path.join(src, _TABLE_INSIGHT_DIR)):
+            file_path = os.path.join(src, _TABLE_INSIGHT_DIR + '/' + filename)
+            table_id = filename.replace('.txt', '').replace('.csv', '')
+            if table_id not in raw_data:
+                raw_data[table_id] = {}
             if filename.endswith('.txt'):
-                raw_data[company]['insights'] = __parse_insight(file_path)
+                raw_data[table_id]['insights'] = _parse_insight(file_path)
             if filename.endswith('.csv'):
-                raw_data[company]['table'] = __parse_table(file_path)
+                raw_data[table_id]['table'] = _parse_table(file_path)
 
-        # merge insights
-        # insights = [{**i, 'company': k} for k, v in raw_data.items() for i in v['insights']]
-        # insights = [[{**i, 'company': k} for i in v['insights']] for k, v in raw_data.items()]
-
-        companies = list(raw_data.keys())
-        train_sep = round(len(companies) * 0.6)
-        val_sep = round(len(companies) * 0.8)
-
+        table_insight_dir = os.path.join(dst, _TABLE_INSIGHT_DIR)
+        os.makedirs(table_insight_dir, exist_ok=True)
         max_doc_length = 0
-        for k, v in raw_data.items():
+        i = 0
+        for k, v in list(raw_data.items()):
+            i += 1
+            if i % 1000 == 0:
+                print('processed {} insights'.format(i))
+
+            out_path = os.path.join(table_insight_dir, '{}.csv'.format(k))
+            if Path(out_path).exists():
+                continue
+            if ('insights' not in v) or len(v['insights']) < 5:
+                del raw_data[k]
+                continue
+
             samples = pd.DataFrame()
             insights = v['insights']
             titles = [i['title'] for i in insights]
             max_doc_length = max((len(t.split()) for t in titles))
             samples['title'] = titles
-            samples['significant'] = [i['sign'] for i in insights]
+            samples['significant'] = [i['sig'] for i in insights]
+            samples['type'] = [_TYPE2ID[i['type']] for i in insights]
             samples['target'] = [i['prob'] for i in insights]
             samples = samples.sample(frac=1).reset_index(drop=True)  # random shuffle
-            samples.to_csv(os.path.join(dst, '{}.csv'.format(k)))
+            samples.to_csv(out_path)
 
         # process vocabulary
-        vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(max_doc_length)
+        min_frequency = self._config['processed']['vocab']['min_frequency']
+        vocab_processor = tflearn.data_utils.VocabularyProcessor(
+            max_doc_length, min_frequency=min_frequency)
+
+        table_ids = list(raw_data.keys())
+        train_sep = round(len(table_ids) * 0.6)
+        val_sep = round(len(table_ids) * 0.8)
 
         # save vocabulary
-        train_titles = [i['title'] for k, v in raw_data.items() if k in companies[:train_sep]
+        train_titles = [i['title'] for k, v in raw_data.items() if k in table_ids[:train_sep]
                         for i in v['insights']]
         vocab_processor.fit(train_titles)
-        vocab_processor.save(os.path.join(dst, self._VOCAB_FILENAME))
+        vocab_processor.save(os.path.join(dst, _VOCAB_FILENAME))
 
-        with open(os.path.join(dst, self._DATASET_META), 'w') as meta_f:
+        with open(os.path.join(dst, _DATASET_META), 'w') as meta_f:
             json.dump({
-                'train': companies[:train_sep],
-                'val': companies[train_sep:val_sep],
-                'test': companies[val_sep:],
+                'train': table_ids[:train_sep],
+                'val': table_ids[train_sep:val_sep],
+                'test': table_ids[val_sep:],
                 'max_doc_length': max_doc_length,
-                'vocab': len(vocab_processor.vocabulary_),
+                'vocab_size': len(vocab_processor.vocabulary_),
             }, meta_f)
 
     def _load_dataset(self):
@@ -82,21 +101,24 @@ class DataPreprocessor(BasicPreprocessor):
 
         task = self._config['train']['task']
         dst = next(node['dst'] for node in self._datasets if node['task'] == task)
-        vocab_path = os.path.join(dst, self._VOCAB_FILENAME)
-        vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor.restore(vocab_path)
+        vocab_path = os.path.join(dst, _VOCAB_FILENAME)
+        vocab_processor = tflearn.data_utils.VocabularyProcessor.restore(vocab_path)
 
-        with open(os.path.join(dst, self._DATASET_META), 'r') as meta_f:
+        with open(os.path.join(dst, _DATASET_META), 'r') as meta_f:
             meta_data = json.load(meta_f)
 
-        def __get_batches(companies):
+        def __get_batches(table_ids):
             __ret = []
-            for company_name in companies:
-                samples = pd.read_csv(os.path.join(dst, '{}.csv'.format(company_name)))
-                batch = {'title': np.array(list(vocab_processor.transform(samples['title']))),
-                         'significant': np.array(samples['significant']),
-                         'target': np.array(samples['target']),
-                         'batch_size': len(samples),
-                         }
+            table_insight_dir = os.path.join(dst, _TABLE_INSIGHT_DIR)
+            for table_id in table_ids:
+                samples = pd.read_csv(os.path.join(table_insight_dir, '{}.csv'.format(table_id)))
+                batch = {
+                    'title': np.array(list(vocab_processor.transform(samples['title']))),
+                    'significant': np.array(samples['significant']),
+                    'type': np.array(samples['type']),
+                    'target': np.array(samples['target']),
+                    'batch_size': len(samples),
+                }
                 __ret.append(batch)
             return __ret
 
@@ -104,6 +126,6 @@ class DataPreprocessor(BasicPreprocessor):
         result['val'] = __get_batches(meta_data['val'])
         result['test'] = __get_batches(meta_data['test'])
         result['max_doc_length'] = meta_data['max_doc_length']
-        result['vocab'] = meta_data['vocab']
+        result['vocab_size'] = meta_data['vocab_size']
 
         return result
